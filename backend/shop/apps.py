1a45bd1fbd96bcbec80ff5d8bfaebd5d2993e989
ByteBard <solomindavid10@gmail.com>
2025-06-14 22:54:33 +0700

    style: localize app and model names in admin panel

--- backend/shop/apps.py
+++ backend/shop/apps.py
@@ -5,6 +5,8 @@ class ShopConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "shop"
 
+    verbose_name = 'Управление магазином'
+
     def ready(self):
         """
         Этот метод вызывается, когда приложение готово.
